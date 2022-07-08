65e3b2d34647a907a9b49b4fc3c4baa6b8e3e34c
tikmart <t.martirosyan012@gmail.com>
2022-07-08 22:10:44 +0400

    Task 4 first commit

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,21 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+
+        int a;
+        int b;
+        float firstBracket;
+        float secondBracket;
+        float result;
+
+        a = 3;
+        b = 5;
+        firstBracket =9 * (float) Math.pow(a,2) - 5*b + 2 + a - 7;
+        secondBracket = (a + b - 4 * a * b) / 2;
+
+        result = firstBracket * secondBracket;
+
+        System.out.println(result);
+
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,18 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+
+        char c = 67;
+        char h = 72;
+        char a = 65;
+        char r = 82;
+
+
+
+        System.out.print(c + " " + h + " " + a + " " + r);
+
+
+
+
     }
 }

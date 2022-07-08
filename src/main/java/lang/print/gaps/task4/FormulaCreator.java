8558530758156bc49b6d13a8c7cc32b5b24c04a0
tikmart <t.martirosyan012@gmail.com>
2022-07-09 00:15:27 +0400

    Task 4 first commit

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -3,18 +3,14 @@ package lang.print.gaps.task4;
 public class FormulaCreator {
     public static void main(String[] args) {
 
-        int a;
-        int b;
-        float firstBracket;
-        float secondBracket;
+        float a;
+        float b;
         float result;
 
         a = 3;
         b = 5;
-        firstBracket =9 * (float) Math.pow(a,2) - 5*b + 2 + a - 7;
-        secondBracket = (a + b - 4 * a * b) / 2;
+        result =(9 * (float) Math.pow(a,2) - 5*b + 2 + a - 7)*((a + b - 4*a*b)/2);
 
-        result = firstBracket * secondBracket;
 
         System.out.println(result);
 
